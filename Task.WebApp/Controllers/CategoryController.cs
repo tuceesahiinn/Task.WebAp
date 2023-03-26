7839b3523e173b888f0fea4ef11f793370615faf
tugce <tugce@dorabilisim.com>
2023-03-27 02:00:56 +0300

    first commit

---
+++ Task.WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.WebApp.Models.Entity;
+
+namespace Task.WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        TaskEntities db = new TaskEntities();
+        // GET: Category
+        public ActionResult GetCategory()
+        {            
+                var categories = db.Category.ToList();
+
+                return View(categories);           
+        }
+        
+    }
+}
---
+++ Task.WebApp/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Task.WebApp.Models.Entity;
+
+namespace Task.WebApp.Controllers
+{
+    public class ProductController : Controller
+    {
+       TaskEntities db = new TaskEntities();
+
+        // GET: Product
+        public ActionResult GetProduct()
+        {
+            var products = db.Product.ToList();
+
+            return View(products);
+        }
+        //GET: Product details
+        public ActionResult GetProductDetail(int id)
+        {
+            var productDetail = db.Product.Where(x => x.Id == id).FirstOrDefault();
+
+            return View("GetProductDetail", productDetail);
+        }
+        // GET: New Products
+        public ActionResult GetNewProduct()
+        {
+            List<Product> newProducts = db.Product.Where(p => p.IsNew == true).ToList();
+
+            return PartialView("_NewProductList", newProducts);
+        }
+    }
+}
